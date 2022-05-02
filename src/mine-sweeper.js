2ce23803b00f53e0757bc152e08f6e9bf418affa
Darya Anikeeva <daanikeeva@gmail.com>
2022-05-02 23:07:46 +0300

    feat: solution mine-sweeper.js, simple-chain.js

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let res = matrix.map(el => el.map(e => 0))
+  matrix.forEach((row, indexRow) => {
+    row.forEach((cell, indexCell) => {
+      let count = 0;
+      if (indexRow === 0) {
+        if (indexCell === 0) {
+          matrix[indexRow][indexCell+1] === true ? count ++ : count;
+          matrix[indexRow+1][indexCell] === true ? count ++ : count;
+          matrix[indexRow+1][indexCell+1] === true ? count ++ : count;
+        }
+        else 
+        if (indexCell === row.length - 1) {
+          matrix[indexRow][indexCell-1] === true ? count ++ : count;
+          matrix[indexRow+1][indexCell-1] === true ? count += 1 : count;
+          matrix[indexRow+1][indexCell] === true ? count += 1 : count;
+        }
+        else {
+          matrix[indexRow][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell+1] === true ? count += 1 : count += 0;
+        }
+      }
+      else if (indexRow === matrix.length - 1) {
+        if (indexCell === 0) {
+          matrix[indexRow][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell+1] === true ? count += 1 : count += 0;
+        }
+        else 
+        if (indexCell === row.length - 1) {
+          matrix[indexRow][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+        }
+        else {
+          matrix[indexRow][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell+1] === true ? count += 1 : count += 0;
+        }
+      }
+      else {
+        if (indexCell === 0) {
+          matrix[indexRow][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell+1] === true ? count += 1 : count += 0;
+        }
+        else 
+        if (indexCell === row.length - 1) {
+          matrix[indexRow][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell] === true ? count += 1 : count += 0;
+        }
+        else {
+          matrix[indexRow][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow-1][indexCell+1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell-1] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell] === true ? count += 1 : count += 0;
+          matrix[indexRow+1][indexCell+1] === true ? count += 1 : count += 0;
+        }
+
+      }
+      res[indexRow][indexCell] = count
+      })
+    })
+    return res
 }
+let matr = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+]
+console.log(minesweeper( matr ))
 
 module.exports = {
   minesweeper
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arrayForChainMaker: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arrayForChainMaker.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink( value ) {
+    if (value === undefined) {
+      this.arrayForChainMaker.push(`( )`);
+      return this
+    }
+    else this.arrayForChainMaker.push(`( ${value + ''} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink( position ) {
+    if (typeof position === 'number' && position >= 1 && position <= this.arrayForChainMaker.length) {
+      this.arrayForChainMaker.splice(position-1, 1);
+      return this
+    }
+    else {
+      this.arrayForChainMaker = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    // return this
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.arrayForChainMaker.reverse();
+    return this
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    let result = this.arrayForChainMaker.join('~~');
+    this.arrayForChainMaker = [];
+    return result
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 };
+
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).reverseChain().finishChain()) // => '( 1 )~~( 2 )~~( 3 )'
+// console.log(chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain())// '( 3rd )~~( function () { } )');
 
 module.exports = {
   chainMaker
